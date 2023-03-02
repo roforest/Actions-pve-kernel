c29fa8449932fd7189a086b441758fb2213ee899
roforest <79748239+roforest@users.noreply.github.com>
2023-03-02 12:24:26 +0800

    update ACS_patch

--- scripts/ACS_patch.sh
+++ scripts/ACS_patch.sh
@@ -1,3 +1,3 @@
 sed -i "s/106 @@/105 @@/g" patches/kernel/0003-pci-Enable-overrides-for-missing-ACS-capabilities-4..patch
-sed -i "/pci_find_ext_capability(dev, PCI_EXT_CAP_ID_ACS))/d" patches/kernel/0003-pci-Enable-overrides-for-missing-ACS-capabilities-4..patch
+sed -i "/pci_find_ext_capability(dev, PCI_EXT_CAP_ID_ACS))/d" patches/kernel/0004-pci-Enable-overrides-for-missing-ACS-capabilities-4..patch
 sed -i "s/(dev) ||/(dev))/g" patches/kernel/0003-pci-Enable-overrides-for-missing-ACS-capabilities-4..patch
