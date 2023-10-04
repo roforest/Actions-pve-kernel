984b5bcb096a50506c63a0855b5344eb9f91ea2b
roforest <79748239+roforest@users.noreply.github.com>
2023-10-04 17:15:25 +0800

    Update free_disk.sh

--- scripts/free_disk.sh
+++ scripts/free_disk.sh
@@ -2,9 +2,12 @@
 apt-get remove -y '^dotnet-.*'
 apt-get remove -y '^llvm-.*'
 apt-get remove -y 'php.*'
+apt-get remove -y '^temurin-.*-jdk'
 apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable microsoft-edge-stable firefox powershell mono-devel
 apt-get autoremove -y
 apt-get clean
 rm -rf /usr/share/dotnet/
 rm -rf /usr/share/swift/
 rm -rf /usr/local/lib/android/
+rm -rf /imagegeneration
+rm -rf /opt/hostedtoolcache/CodeQL/
