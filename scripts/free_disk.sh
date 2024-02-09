12b6c73b3d959a9145f18b91d5a3aee5c6fd9df8
roforest <79748239+roforest@users.noreply.github.com>
2024-02-09 19:50:46 +0800

    Free More Space

--- scripts/free_disk.sh
+++ scripts/free_disk.sh
@@ -1,21 +1,23 @@
-apt-get remove -y '^ghc*'
-apt-get remove -y '^dotnet-.*'
-apt-get remove -y '^llvm-.*'
-apt-get remove -y 'php.*'
-apt-get remove -y '^temurin-.*-jdk'
-apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable microsoft-edge-stable firefox powershell mono-devel
+apt purge -y '^ghc*'
+apt purge -y '^dotnet-.*'
+apt purge -y '^llvm-.*'
+apt purge -y 'php.*'
+apt purge -y '^temurin-.*-jdk'
+apt purge -y azure-cli google-cloud-sdk hhvm google-chrome-stable microsoft-edge-stable firefox powershell mono-devel mongodb* libpq-dev moby-* cabal* clang*
 apt-get autoremove -y
-apt-get clean
+apt-get autoclean -y
+rm -rf /opt/*
+rm -rf /imagegeneration
 rm -rf /usr/share/dotnet/
 rm -rf /usr/share/swift/
-rm -rf /usr/local/lib/android/
-rm -rf /imagegeneration
-rm -rf /opt/hostedtoolcache/CodeQL/
-rm -rf /usr/local/julia*
 rm -rf /usr/share/kotlinc
+rm -rf /usr/share/miniconda
+rm -rf /usr/share/sbt
+rm -rf /usr/local/lib/android/
+rm -rf /usr/local/julia*
 rm -rf /usr/local/share/phantomjs*
 rm -rf /usr/local/share/boost
-rm -rf /usr/share/miniconda
+rm -rf /usr/lib/jvm
 docker rmi $(docker image ls -q --filter "reference=node*")
 docker rmi $(docker image ls -q --filter "reference=buildpack*")
 docker rmi $(docker image ls -q --filter "reference=alpine*")
